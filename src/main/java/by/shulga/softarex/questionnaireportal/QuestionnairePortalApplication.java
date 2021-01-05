f2f8870a4c473cce5341897b84a97f1dbe924075
sasha451 <shulga.sasha.new@gmail.com>
2021-01-05 22:15:05 +0300

    Init commit

---
+++ src/main/java/by/shulga/softarex/questionnaireportal/QuestionnairePortalApplication.java
@@ -0,0 +1,13 @@
+package by.shulga.softarex.questionnaireportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionnairePortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuestionnairePortalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/shulga/softarex/questionnaireportal/QuestionnairePortalApplicationTests.java
@@ -0,0 +1,13 @@
+package by.shulga.softarex.questionnaireportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionnairePortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
