c37ef51ca7524e073fb7a9a027e263be100be1a4
sasha451 <shulga.sasha.new@gmail.com>
2021-01-20 01:25:48 +0300

    Fix field delete

--- src/main/java/by/shulga/softarex/questionnaireportal/web/WebSocketController.java
+++ src/main/java/by/shulga/softarex/questionnaireportal/web/WebSocketController.java
@@ -4,14 +4,10 @@ import by.shulga.softarex.questionnaireportal.dto.ResponseDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
-import java.util.Map;
-import com.google.gson.Gson;
 @Controller
 public class WebSocketController {
 
